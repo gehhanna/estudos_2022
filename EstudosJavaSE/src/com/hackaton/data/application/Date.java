44e3bb63262d6edc36ab34f1102fb70efea30172
George Hanna <ge.hanna2015@gmail.com>
2022-02-24 14:46:10 -0300

    formatarHoraDataAtual

--- EstudosJavaSE/src/com/hackaton/data/application/Date.java
+++ EstudosJavaSE/src/com/hackaton/data/application/Date.java
@@ -2,20 +2,35 @@ package com.hackaton.data.application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class Date {
 
-public class Date   { 
+    public static void main(String[] args) throws ParseException {
 
-public static void main(String[] args) throws ParseException {
+        // data/hora atual
+        LocalDateTime agora = LocalDateTime.now();
 
-    SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
-    SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        // formatar a data
+        DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+        String dataFormatada = formatterData.format(agora);
 
-    java.util.Date y1 = sdf1.parse("25/06/2018");
-    java.util.Date y2 = sdf2.parse("25/06/2018 15:45:35");
+        // formatar a hora
+        DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String horaFormatada = formatterHora.format(agora);
 
-    System.out.println("y1: " + sdf2.format(y1));
-    System.out.println("y2: " + sdf2.format(y2));;
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        java.util.Date y1 = sdf1.parse("24/02/2022");
+        java.util.Date y2 = sdf2.parse("24/02/2022 15:45:35");
+        System.out.println("horaAtual: " + (horaFormatada));
+        System.out.println("dataAtual: " + (dataFormatada));
+
+        System.out.println("y1: " + sdf2.format(y1));
+        System.out.println("y2: " + sdf2.format(y2));
+        ;
 
     }
 
