88fd4a3195fb8f65c79c7af4c9b9bafa5abea463
George Hanna <ge.hanna2015@gmail.com>
2022-02-24 15:07:50 -0300

    dataHoraEST

--- EstudosJavaSE/src/com/hackaton/data/application/Date.java
+++ EstudosJavaSE/src/com/hackaton/data/application/Date.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 public class Date {
 
@@ -22,14 +23,24 @@ public class Date {
 
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        // converter as horas para a timeZone de certa região neste caso EST. Horário Padrão Oriental.
+        sdf3.setTimeZone(TimeZone.getTimeZone("EST"));
 
+        // aqui dois exemplos um somente  data "sdf1" e outro com data e hora "sdf2" simpleDateFormat.
         java.util.Date y1 = sdf1.parse("24/02/2022");
         java.util.Date y2 = sdf2.parse("24/02/2022 15:45:35");
+        // Aqui a impressão da hora atual e data atual.
         System.out.println("horaAtual: " + (horaFormatada));
         System.out.println("dataAtual: " + (dataFormatada));
 
         System.out.println("y1: " + sdf2.format(y1));
         System.out.println("y2: " + sdf2.format(y2));
+        //Horas convertidas para EST.
+        System.out.println("------------------------");
+        System.out.println("y1: " + sdf3.format(y1));
+        System.out.println("y2: " + sdf3.format(y2));
+        
         ;
 
     }
